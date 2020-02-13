0415a55f51c9b19458964cbf80d1e4aa1f4a67d5
TobiOladoja <tbfullstack@gmail.com>
2020-02-13 14:32:07 +0000

    Using the Effect Hook Example 1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `clicked ${count} times`;
+  });
   return (
     <div>
       <p>You clicked {count} times</p>
